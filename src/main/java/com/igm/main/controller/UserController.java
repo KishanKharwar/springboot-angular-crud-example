3b21b7fed5fa9804b8d6cb0612c9be3441ffc759
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-04-11 17:44:51 +0530

    Initial Commit

---
+++ src/main/java/com/igm/main/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.igm.main.controller;
+
+import com.igm.main.entities.User;
+import com.igm.main.repositories.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/users")
+  public List<User> getUsers() {
+    return (List<User>) userRepository.findAll();
+  }
+
+  @PostMapping("/users")
+  void addUser(@RequestBody User user) {
+    userRepository.save(user);
+  }
+}
---
+++ src/main/java/com/igm/main/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.igm.main.repositories;
+
+import com.igm.main.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
